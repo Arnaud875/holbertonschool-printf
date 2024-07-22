7c3156a1b50945fc11fa823e706687d02359ab27
Arnaud875 <arnaud.nickles33@gmail.com>
2024-07-22 14:42:21 +0200

    Make printf function

--- main.h
+++ main.h
@@ -1,7 +1,18 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
+#include <stdio.h>
 #include <stdarg.h>
+
+/**
+ * struct PrintType - A structure to print a specific type
+ * @type: The type
+ */
+typedef struct PrintType
+{
+    char *type;
+    int (*print_type_function)(va_list args);
+} PrintType_t;
 
 int _printf(const char *format, ...);
 int _putchar(char character);

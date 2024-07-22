6d9fb72c48fbbe9afb0e557d2d08d3551a97274f
Arnaud875 <arnaud.nickles33@gmail.com>
2024-07-22 10:15:46 +0200

    Creating the printf project base

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - Format a string and print it in the console
+ * @format: The string formatted with variadic arguments
+ * @...: The variadic arguments
+ * Return: Number of bytes printed in the console
+ * without the null byte.
+ */
+int _printf(const char *format, ...)
+{
+}

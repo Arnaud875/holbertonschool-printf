8d89e8d51080a202deba85357f16fa3ae2e0aae4
Duupain <emadecot03@gmail.com>
2024-07-24 10:40:45 +0200

    Fix betty on print_binary file

--- print_binary.c
+++ print_binary.c
@@ -7,33 +7,33 @@
  */
 int print_binary(va_list arguments)
 {
-  unsigned int num = va_arg(arguments, unsigned int);
-  int count = 0;
-  int i;
-  unsigned int mask = 1 << (sizeof(unsigned int) * 8 - 1);
-  int started = 0;
+	unsigned int num = va_arg(arguments, unsigned int);
+	int count = 0;
+	int i;
+	unsigned int mask = 1 << (sizeof(unsigned int) * 8 - 1);
+	int started = 0;
 
-  for (i = 0; i < (int)(sizeof(unsigned int) * 8); i++)
-  {
-    if (num & mask)
-    {
-      putchar('1');
-      started = 1;
-      count++;
-    }
-    else if (started)
-    {
-      putchar('0');
-      count++;
-    }
-    mask >>= 1;
-  }
+	for (i = 0; i < (int)(sizeof(unsigned int) * 8); i++)
+	{
+		if (num & mask)
+		{
+			putchar('1');
+			started = 1;
+			count++;
+		}
+		else if (started)
+		{
+			putchar('0');
+			count++;
+		}
+		mask >>= 1;
+	}
 
-  if (count == 0)
-  {
-    putchar('0');
-    count++;
-  }
+	if (count == 0)
+	{
+		putchar('0');
+		count++;
+	}
 
-  return count;
+	return count;
 }
